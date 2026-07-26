1a20b5a096441947dc641bdaf25d2d49c320d108
brandonstevenz2002 <brandonstevenvm@gmail.com>
2026-07-25 19:57:32 -0500

    .

--- App.js
+++ App.js
@@ -1,14 +1,1 @@
-function Mybutton() {
-    return (
-        <button>Soy un botón</button>
-    );
-}
-
-export default function Myapp() {
-    return (
-        <div>
-            <h1>Bienvenido a mi aplicación</h1>
-            <Mybutton />
-        </div>
-    )
-}
+<img className="avatar" />
