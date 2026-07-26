018172dd1465bb964d8507e5db1f9b73e967caaf
brandonstevenz2002 <brandonstevenvm@gmail.com>
2026-07-25 20:03:21 -0500

    .

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-return (
-    <h1>
-        {user.name}
-    </h1>
-);
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOVdOSs.jpg',
+    imageSize: 90,
+};
+
+export default function profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'foto de ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
