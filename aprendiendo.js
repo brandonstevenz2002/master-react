646e8d79f9776d7979812097ad15c89528506860
brandonstevenz2002 <brandonstevenvm@gmail.com>
2026-07-25 19:39:08 -0500

    .

--- aprendiendo.js
+++
@@ -1,5 +0,0 @@
-function Mybutton() {
-    return (
-        <button>Soy un botón</button>
-    );
-}
---
+++ aprendiendo.jsx
@@ -0,0 +1,8 @@
+export default function Myapp() {
+    return(
+        <div>
+            <h1>Bienvenido a mi aplicación</h1>
+            <Mybutton />
+        </div>
+    )
+}
